07fd615695dbbfda9a9488117d28f93d8340ef40
Sharanvj1997 <saranvj1230@gmail.com>
2026-05-21 16:35:36 +0530

    Implemented CSV upload API

--- ETL/settings.py
+++ ETL/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'uploads',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- uploads/views.py
+++ uploads/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import UploadedFile
+from .serializers import UploadedFileSerializer
+
+class FileUploadAPIView(APIView):
+    def post(self, request):
+
+        uploaded_file = request.FILES.get('file')
+
+        if not uploaded_file:
+            return Response(
+                {"error": "No file uploaded"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        file_instance = UploadedFile.objects.create(
+            file=uploaded_file,
+            filename=uploaded_file.name
+        )
+
+        serializer = UploadedFileSerializer(file_instance)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
